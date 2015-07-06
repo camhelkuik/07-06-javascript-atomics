7b060ec72e1b21970174ac24cd2081f869325ba9
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-06 15:01:09 -0500

    Finished 6B

---
+++ if-statements/6B/global.js
@@ -0,0 +1,9 @@
+var password= "doofus"
+
+var answer= prompt("Please enter the password:");
+
+if (answer === password){
+  alert("Great job!");
+} else {
+  alert("Sorry, the password is actually doofus");
+}
