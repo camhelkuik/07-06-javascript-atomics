3f24c747e6e3e83ebc28339ac0064547c66fae09
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-06 15:14:29 -0500

    Finished part 6C

---
+++ if-statements/6C/global.js
@@ -0,0 +1,9 @@
+var number= 47;
+var answer= prompt("What is the secret number:");
+answer= parseInt(answer)
+
+if (answer === number){
+  alert("Great job!");
+} else {
+  alert("Sorry, the secret number is 47");
+}
