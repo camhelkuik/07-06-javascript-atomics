759c17e73fb21628451cff5f57834cd2b99741ce
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-06 11:33:51 -0500

    Finished part 2

---
+++ js-output/global.js
@@ -0,0 +1,20 @@
+//Greeting 
+alert("Hello! My name is Cameron!");
+
+//What my favorite breakfast is
+console.log("My favorite breakfast is eggs and sausage.");
+
+//How many cats I live with
+console.log("I have one cat name Luna and she is a Sphynx cat.");
+
+//Favorite place to eat lots of food
+console.log("My favorite place to eat LOTS of food is Mother Inida.");
+
+//How many naps I take per week
+console.log("I take about 5 naps per week.");
+
+//How many naps I would like to take
+console.log("I would like to take 0 naps per week.");
+
+//Goodbye
+alert("Goodbye!");
