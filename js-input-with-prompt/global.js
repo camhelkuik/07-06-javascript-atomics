b2307920fb3b19fb440f35f49c426adc5b2a53a3
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-06 13:00:15 -0500

    Finished part 4.

---
+++ js-input-with-prompt/global.js
@@ -0,0 +1,19 @@
+var name= prompt("What is your name?");
+var age= prompt("What is your age?");
+var hometown= prompt("What is your hometown?");
+var favNumber= prompt("What is your favorite number?");
+
+alert("Your name is " + name);
+console.log(name);
+
+alert("Your age is " + age);
+console.log(age);
+
+alert("Your hometown is " + hometown);
+console.log(hometown);
+
+alert("Your favorite number is " + favNumber);
+console.log(favNumber);
+
+alert(typeof age);
+alert(typeof name);
