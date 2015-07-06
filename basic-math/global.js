81f2dd769a2dac456bf4cbaa6c06dbd1f0984cc5
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-06 13:56:23 -0500

    Finished part 5.

---
+++ basic-math/global.js
@@ -0,0 +1,24 @@
+var number1= prompt("Please enter your first number:");
+number1= parseInt(number1);
+
+var number2= prompt("Please enter another number:");
+number2= parseInt(number2);
+
+var number3= prompt("Please enter the last number:");
+number3= parseInt(number3);
+
+var sumTotal= number1  + number2 + number3;
+alert(number1 + " + " + number2 + " + " + number3 + " = " + sumTotal);
+
+var subTotal= number1 - number2 - number3
+alert(number1 + " - " + number2 + " - " + number3 + " = " + subTotal);
+
+var productTotal= number1 * number2 * number3;
+alert(number1 + " * " + number2 + " * " + number3 + " = " + productTotal);
+
+var quotientTotal= number1 / number2;
+alert(number1 + " / " + number2 + " = " + quotientTotal);
+
+alert(number1 + " ++ " + " = " + ++number1);
+
+alert(number2 + " -- " + " = " + --number2);
